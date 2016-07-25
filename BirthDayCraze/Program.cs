c2d498070f7a9995c77007431955b2b4e1c7a66c
Jarell Bennett <jb123506@gmail.com>
2016-07-25 03:19:33 -0400

    first

---
+++ BirthDayCraze/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthDayCraze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first name");
+            string name = Console.ReadLine();                   //Enter first name
+
+            Console.WriteLine("Enter your month of birth");
+            string month = Console.ReadLine().ToLower();         //Enter month that gets converted to lower case version
+
+            Console.WriteLine('\n'+bDay(name,month));            //Writes name + month after passing through method
+
+        }
+
+        //Reverse method
+        static string bDay(string j, string k)
+        {
+            string final = "";
+
+            if(k == "april" || k == "august" || k == "october" )         //check if user entered one of the months
+            {
+                Console.WriteLine(j + " is an " + k + " baby");
+            }
+            else if(k == "apr" || k == "aug" || k == "oct")                 //check for abbreviated forms of months
+            {
+                Console.WriteLine(j + " is an " + k + " baby");
+            }
+            else
+            {
+                Console.WriteLine(j + " is a " + k + " baby");
+            }
+            return final;                                                   //return updated string final
+        }
+    }
+}
